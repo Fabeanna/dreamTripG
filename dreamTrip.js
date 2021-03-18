0edfbd6acd1567f6a45088724d7eca0e06bbc7c7
fabeanna ward <fabeanna.w1820@hotmail.com>
2021-03-17 21:51:42 -0400

    final commit 1

--- dreamTrip.js
+++ dreamTrip.js
@@ -1,2 +1,24 @@
-let name ="mike";
-{console.log(name);}
+let ModeTran = [car,suv, limo, train ,bus]
+let denstenation = [paris, nashville, toyko, milan, london]
+let Resteurant = [bbq, french, sushi, italian, wellingtons]
+let Entertainment = [Concert, PaintingSess, shoppingSpree, kaorke, tourOfCity]
+
+function randomMoTran (arrayMoTran); {
+
+for(let arrayMoTran = 0; index < arrayMoTran.length; index++) {
+    if arrayMoTran.[inedx] === ("car") 
+    {console.log("A Car Has been Selected for you");}
+    // can a prompt be used here?
+    else {console.log("There are more choices to be had!")}
+}
+let myResult = randomMoTran (arrayMoTran);
+
+)
+// steps needed
+// have code random;ly select city
+// prompt user to agree with city
+// if user agrees move to next function if user disagrees run previous code again
+//repeat random generator for all arrays untill user has agree to all arrays
+// alert user of choices, have them agree again. end with thank you
+
+
